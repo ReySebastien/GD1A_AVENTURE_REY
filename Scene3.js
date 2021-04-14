af0f27477b84f55b1a8ff4fc43669203db470976
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-14 20:49:31 +0200

    tests

--- Scene3.js
+++ Scene3.js
@@ -6,13 +6,61 @@ class Scene3 extends Phaser.Scene{
 preload(){
     
     this.load.image('fond_test_2', 'assets_test/fond_test_2.png');
+    this.load.image('perso_test', 'assets_test/perso_test.png');
     
 } // FIN PRELOAD
     
 create(){
         
     this.add.image(960,540, 'fond_test_2');
-
+    this.player = this.physics.add.image(960, 540, 'perso_test');
+    this.player.setCollideWorldBounds(true);
+    this.cursors = this.input.keyboard.createCursorKeys();
 } // FIN CREATE
+    
+update(){
+        
+    if (this.cursors.left.isDown)
+    {
+        
+        this.player.setVelocityX(-500);
+        
+    }
+    else if (this.cursors.right.isDown)
+    {
+        
+        this.player.setVelocityX(500);
+
+    }
+    else
+    {
+        
+        this.player.setVelocityX(0);  
+        
+    }
+    
+    if(this.cursors.up.isDown)
+    {
+            
+        this.player.setVelocityY(-500);
+    
+    }
+    
+        
+    else if (this.cursors.down.isDown)
+    {
+        
+        this.player.setVelocityY(500)
+        
+    }
+        
+    else
+    {
+        
+        this.player.setVelocityY(0);  
+        
+    }
+        
+    } // FIN UPDATE
     
 } // FIN DE LA CLASSE
