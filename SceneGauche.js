fc203b7b284e611c0eb58cae546e8b06f0bb496b
ReySebastien <74655004+ReySebastien@users.noreply.github.com>
2021-04-23 14:36:02 +0200

    essai fonction hitEnnemi

--- SceneGauche.js
+++ SceneGauche.js
@@ -8,6 +8,7 @@ preload(){
     this.load.image('fond_test_2', 'assets_test/fond_test_2.png');
     this.load.image('perso_test', 'assets_test/perso_test.png');
     this.load.image('bordure_droite2', 'assets_test/bordure_test.png');
+    this.load.image('ennemi_test', 'assets_test/ennemi_test.png');
     
 } // FIN PRELOAD
     
@@ -16,9 +17,12 @@ create(){
     this.add.image(960,540, 'fond_test_2');
     this.player = this.physics.add.image(960, 540, 'perso_test');
     this.player.setCollideWorldBounds(true);
+    this.ennemi = this.physics.add.image(500, 540, 'ennemi_test');
+    this.ennemi.setCollideWorldBounds(true);
     this.cursors = this.input.keyboard.createCursorKeys();
     var bordure_droite2 = this.physics.add.image(1919,540, 'bordure_droite2');
     this.physics.add.collider(this.player, bordure_droite2, this.hitBordureDroite2, null, this);
+    this.physics.add.overlap(this.player, this.ennemi, this.hitEnnemi, null, this);
 } // FIN CREATE
     
 update(){
@@ -73,5 +77,25 @@ update(){
         this.cursors.left.isDown = false;
 
      }
+    
+    hitEnnemi(player, ennemi){
+     
+    if (!this.invulnerabilite){
+        this.vie -= 1;
+        this.invulnerabilite = true;
+        
+        if(this.vie > 0){
+            clignotement = this.time.addEvent({ delay : 200, repeat: 9, callback: function(){this.player.visible = !this.player.visible;}, callbackScope: this});
+        }
+        
+        //tempsInvulnerabilite = this.time.addEvent({ delay : 2000, callback: function(){this.invulnerabilite = false}, callbackScope: this});
+    }
+     
+     if(this.vie == 0){
+        this.player.setTint(0xff0000);
+        this.physics.pause();
+        this.gameOver = true;
+    }
+ }
     
 } // FIN DE LA CLASSE
